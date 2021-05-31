2362c8be522a202eaa76a888703aa8f2a2313386
Christian Parker <christianparker512@gmail.com>
2021-05-31 14:36:39 -0500

    another quotation separatoin eexample

--- escapecharacter.py
+++ escapecharacter.py
@@ -4,4 +4,6 @@ print(splitString)
 tabbedString ="1\t2\t3\t4\t5"
 print(tabbedString)
 
-print ('The pet shop owner "No, no, \'e\'s uh, he\'s resting".')
+print ('The pet shop owner "Nogit, no, \'e\'s uh, he\'s resting".')
+
+print("The pet shop owner said \"No,no, 'e's uh, ....he's resting\".')")
