a48366fc75a68db65d13ddfcdf54e85e06952335
Christian Parker <christianparker512@gmail.com>
2021-05-31 11:12:01 -0500

    input and concatanation

--- strings.py
+++ strings.py
@@ -2,3 +2,9 @@ print("today is a good day to learn python")
 print("python is fun")
 print("python's string are easy to use")
 print('we can even include "quotes" in strings')
+print("Hello" + " world")
+
+greeting = "hello"
+# name ="Chris"
+name = input("Please enter your name")
+print(greeting + ' ' + name)
