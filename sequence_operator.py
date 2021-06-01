009a8a96e37a5b422afcb72c70a7a2d14eb0d51a
Christian Parker <christianparker512@gmail.com>
2021-06-01 14:38:53 -0500

    printing characters in strings

--- sequence_operator.py
+++ sequence_operator.py
@@ -6,3 +6,9 @@ string5="football season."
 print(string1 + string2 + string3 + string4 + string5)
 
 print("Hello " * 5)
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print ("thur" in today)
+print("parrot" in "football")
