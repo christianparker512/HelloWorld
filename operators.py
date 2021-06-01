7ce553dcb27ed164e737eb73d8807b1bd1df64f8
Christian Parker <christianparker512@gmail.com>
2021-06-01 11:08:11 -0500

    expression precedence

--- operators.py
+++ operators.py
@@ -6,14 +6,16 @@ print(a - b)
 print(a * b)
 print(a/b)
 print(a//b)
-print(a %b)
+print(a % b)
+
 print ()
 
-for i in range(1, a//b):
-    print(i)
-print()
+print(a + b / 3-4 *12)
 
-i = 1
-print(i)
-i =2
-print(i)
+print ((((a + b)/3)-4)*12)
+
+c = a + b
+d = c/3
+e = d-4
+print (e *12)
+print (a/(b *a)/b)
