10d7bffa874e99e498e70cc66c747383b73dfce0
Christian Parker <christianparker512@gmail.com>
2021-06-01 11:56:42 -0500

    split and join a string

--- strings2.py
+++ strings2.py
@@ -4,3 +4,13 @@ print(parrot)
 
 print(parrot[-4:-2])
 print(parrot[-4:12])
+
+#step
+print(parrot[0:6:2])
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
