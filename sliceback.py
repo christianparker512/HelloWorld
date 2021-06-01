9f343ef7ae616d2807de39d75aebed24f5dcb349
Christian Parker <christianparker512@gmail.com>
2021-06-01 12:09:17 -0500

    Slice idioms

--- sliceback.py
+++ sliceback.py
@@ -2,3 +2,10 @@ letters = "abcdefghijklmnopqrstuvwxyz"
 
 backwards = letters[::-1]
 print(backwards)
+
+print(letters[16:13:-1])
+print(letters[4::-1])
+print(letters[:-9:-1])
+
+print(letters[-4:])
+print(letters[-1])
