4eb9a25e9f8d76fc477efb1badc311e737e8e308
Christian Parker <christianparker512@gmail.com>
2021-06-01 11:35:47 -0500

    final slice

--- strings2.py
+++ strings2.py
@@ -2,36 +2,5 @@
 parrot = "Norwegian Blue"
 print(parrot)
 
-print(parrot[13])
-
-print(parrot[3])
-print(parrot[4])
-print()
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
-
-print(parrot[-3])
-print(parrot[-4])
-print()
-print(parrot[-3])
-print(parrot[-6])
-print(parrot[-8])
-
-
-print(parrot[3 -14])
-print(parrot[4 -14])
-print()
-print(parrot[3 -14])
-print(parrot[6-14])
-print(parrot[8-14])
-
-print(parrot[0:6]) #slice
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
-
-print(parrot[10:14])
-print(parrot[6:])
-print(parrot[:6])
-print(parrot[:])
+print(parrot[-4:-2])
+print(parrot[-4:12])
