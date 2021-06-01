73b1c183d77ecce7a989655339d1fe6dd4abd978
Christian Parker <christianparker512@gmail.com>
2021-06-01 14:46:29 -0500

    replacement field number 2

--- repfield.py
+++ repfield.py
@@ -1,2 +1,4 @@
 age = 50
 print("My age is {0} years".format(age))
+
+print("there are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, and {7}".format(31, "jan", "march", "may", "jul", "aug", "oct", "dec"))
