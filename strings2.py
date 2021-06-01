1be2bbc53680e68c502b7d8e2d2c6d8bf98a018b
Christian Parker <christianparker512@gmail.com>
2021-06-01 11:32:18 -0500

    slicing with positive and negative indexes and concatenating

--- strings2.py
+++ strings2.py
@@ -28,3 +28,10 @@ print(parrot[8-14])
 
 print(parrot[0:6]) #slice
 print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+
+print(parrot[10:14])
+print(parrot[6:])
+print(parrot[:6])
+print(parrot[:])
